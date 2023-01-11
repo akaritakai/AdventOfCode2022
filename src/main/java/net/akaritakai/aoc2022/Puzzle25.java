9d0b8c5384de3cc43dc4beaacb9a25da9309e00c
Justin Kaufman <akaritakai@gmail.com>
2023-01-10 18:24:31 -0600

    Simplification to Day 25

--- src/main/java/net/akaritakai/aoc2022/Puzzle25.java
+++ src/main/java/net/akaritakai/aoc2022/Puzzle25.java
@@ -1,11 +1,13 @@
 package net.akaritakai.aoc2022;
 
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.primitives.Chars;
 
 /**
  * In Day 25, we're summing up base 5 numbers where some digits can be negative.
  */
 public class Puzzle25 extends AbstractPuzzle {
+    private static final char[] DIGITS = {'=', '-', '0', '1', '2'};
 
     public Puzzle25(String puzzleInput) {
         super(puzzleInput);
@@ -13,10 +15,9 @@ public class Puzzle25 extends AbstractPuzzle {
 
     @VisibleForTesting
     static String toSnafu(long n) {
-        var digits = new char[]{'=', '-', '0', '1', '2'};
         var sb = new StringBuilder();
         do {
-            sb.append(digits[(int) ((n + 2) % 5)]);
+            sb.append(DIGITS[(int) ((n + 2) % 5)]);
             n = (n + 2) / 5;
         } while (n != 0);
         return sb.reverse().toString();
@@ -26,14 +27,7 @@ public class Puzzle25 extends AbstractPuzzle {
     static long fromSnafu(String snafu) {
         long n = 0;
         for (var c : snafu.toCharArray()) {
-            n *= 5;
-            switch (c) {
-                case '2' -> n += 2;
-                case '1' -> n += 1;
-                case '0' -> n += 0;
-                case '-' -> n -= 1;
-                case '=' -> n -= 2;
-            }
+            n = (5 * n) + Chars.indexOf(DIGITS, c) - 2;
         }
         return n;
     }
